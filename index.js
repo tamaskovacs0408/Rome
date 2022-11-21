26afa42e773575ca2f451ba9f1a8d94ae0e23fa5
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-11-21 11:28:08 +0100

    Add path to static files

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(helmet());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.listen(port, () => {
