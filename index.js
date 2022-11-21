6a363f8b5a3088447c8d6fa5875b3347f52bb866
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-11-21 11:57:18 +0100

    Set up routes

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get("/colosseum", (req, res) => {
+  res.render("colosseum");
+});
+
+app.get("/forum", (req, res) => {
+  res.render("forum");
+});
+
+app.get("/pantheon", (req, res) => {
+  res.render("pantheon");
+});
+
+app.get("/trevi", (req, res) => {
+  res.render("trevi");
+});
+
+app.get("/castle", (req, res) => {
+  res.render("castle");
+});
+
+app.get("/vatican", (req, res) => {
+  res.render("vatican");
+});
 
 app.listen(port, () => {
   console.log(`Server runs at port ${port}.`)
